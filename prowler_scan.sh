cdfb3962018d325e7ba75e347782108bd4b149fa
Cameron Covington <camcov@amazon.com>
2023-04-05 16:32:27 -0400

    Added functionality for visualization generation

--- prowler_scan.sh
+++ prowler_scan.sh
@@ -292,10 +292,14 @@ grep -v -i 'Access Denied getting bucket\|Access Denied Trying to Get\|InvalidTo
 echo "Completed."
 echo ""
 
+#Generate visualizations
+echo "Generating visualizations..."
+/usr/bin/python3 /usr/local/prowler/generateVisualizations.py
+
 #Zip output results into a single file for download (stdout-* includes stdout and can be reviewed for troubleshooting)
 OUTPUT_SUFFIX=$(date +%m-%d-%Y-%H-%M)
 echo "Zipping output results into a single file for download. Output File: prowler_output.zip"
-zip prowler_output-$OUTPUT_SUFFIX.zip output/*.csv output/*.txt output/*.json output/*.html
+zip -r prowler_output-$OUTPUT_SUFFIX.zip output/*.csv output/*.txt output/*.json output/*.html output/ResultsVisualizations-*/*.*
 echo "Completed."
 echo ""
 
